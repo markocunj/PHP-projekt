68ca0cd2d94f362704a85187c0fe03899c34114f
Marko Cunj <marko.cunj@gmail.com>
2021-12-28 13:21:44 +0100

    Rjesenje 1. i 2. zadatka PHP dijela projekta

---
+++ about.php
@@ -0,0 +1,26 @@
+<?php
+print '
+    <main>
+      <div class="container">
+        <br />
+        <h1>About us</h1>
+        <hr />
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe
+            class="embed-responsive-item"
+            src="https://www.youtube.com/embed/HdPzOWlLrbE"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <hr />
+        <p>
+          The widely accepted theory for the origin and evolution of the
+          universe is the Big Bang model, which states that the universe began
+          as an incredibly hot, dense point roughly 13.7 billion years ago. Its
+          our job and our hobby to explore origin of the universe as well as its
+          fundamental secrets.
+        </p>
+        <hr />
+      </div>
+    </main>';
+?>
---
+++ gallery.php
@@ -0,0 +1,48 @@
+<?php
+print '
+    <main>
+      <div class="container">
+        <h1>Gallery</h1>
+        <hr />
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <figure class="figure">
+              <img src="img/gallery-1.jpg" class="figure-img img-fluid rounded" />
+              <figcaption class="figure-caption">
+                A caption for the above image.
+              </figcaption>
+            </figure>
+          </div>
+          <div class="col-md-4">
+            <figure class="figure">
+              <img src="img/gallery-2.jpg" class="figure-img img-fluid rounded" />
+              <figcaption class="figure-caption">
+                A caption for the above image.
+              </figcaption>
+            </figure>
+          </div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <figure class="figure">
+              <img src="img/gallery-3.jpg" class="figure-img img-fluid rounded" />
+              <figcaption class="figure-caption">
+                A caption for the above image.
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <figure class="figure">
+              <img src="img/gallery-4.jpg" class="figure-img img-fluid rounded" />
+              <figcaption class="figure-caption">
+                A caption for the above image.
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </main>';
+?>
